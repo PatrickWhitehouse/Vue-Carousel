17b5db3398dc569f0df041a726c5d87cbca22c55
Patrick Whitehouse <patrick1996@gmail.com>
2019-09-18 19:54:22 +0100

    add json data, assign to variable products

--- carousel/src/App.vue
+++ carousel/src/App.vue
@@ -2,17 +2,24 @@
   <div id="app">
     <h1>Distrelec Task - Carousel</h1>
     <Product name="Test" price="30" url="lorem"/>
+    <p>{{products.numberOfItems}}</p>
   </div>
 </template>
 
 <script>
 
 import Product from "./components/Product";
+import JSON from "./assets/data.json";
 
 export default {
   name: 'app',
   components: {
     Product
+  },
+  data(){
+    return{
+      products: JSON
+    }
   }
 }
 </script>
